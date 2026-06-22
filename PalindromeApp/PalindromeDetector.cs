c96968882e867b6c8ea25579cee55a663b81f90d
DtgfjtwpXehpehp <sivuyilemtwetwe@gmail.com>
2026-06-22 16:00:36 +0200

    Add project files.

---
+++ PalindromeApp/PalindromeDetector.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PalindromeApp
+{
+    internal class PalindromeDetector
+    {
+        private string Reverse(string userInput)
+        {
+            string reversedText = "";
+
+            for (int i = userInput.Length - 1; i >= 0; i--)
+            {
+                reversedText = reversedText + userInput[i];
+            }
+
+            return reversedText;
+        }
+
+        public bool isPalindrome(string potentialPalindrome) {
+            string reversedText = Reverse(potentialPalindrome);
+
+            if (reversedText == potentialPalindrome)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
---
+++ PalindromeApp/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace PalindromeApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput = string.Empty;
+            while (userInput != "/exit")
+            {
+
+            Console.WriteLine("Enter a word that you want to check if it's a palindrome: ");
+             userInput = Console.ReadLine();
+
+            PalindromeDetector detector = new PalindromeDetector();
+
+            bool result = detector.isPalindrome(userInput);
+
+
+            Console.WriteLine($"{userInput} -> {result}");
+            }
+        }
+    }
+}
